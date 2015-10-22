4edbc67bd7983387b73956c22b44855b2ef39bd0
HanCao <hank9cao@gmail.com>
2015-10-22 13:39:58 +0200

    modify preprocess

--- python/preprocess.py
+++ python/preprocess.py
@@ -7,11 +7,18 @@ import matplotlib.pyplot as plt
 
 
 
-#@data: data structure holding all data
-#@address1, file address of hospital data
-#@address2, file address of weather data
 
-def loading_data(paras, data, address1, address2):
+
+def load_hospital_data(paras, data, address1):
+    dates = data["date1"]
+    hos = data["hospital"]
+    ys = data["ylabels"]
+    X = data["X"]
+    y = data["y"]
+    city = data["city"]
+    ss = data["season_start"]
+
+    
     l=[]
     with open(address1) as csvfile:
         r = csv.DictReader(csvfile)
@@ -27,51 +34,64 @@ def loading_data(paras, data, address1, address2):
 
     #insert date data
     for i in newl:
-        data["date1"].append(i[''])
+        dates.append(i[''])
         del i['']
 
     #load city names
-    data["city"] = newl[0].keys()
-    
+    city = newl[0].keys()
+
+
     #init ylabel and date 
-    data["hospital"] = np.zeros(shape=(len(data['date1']), \
-                                       len(data["city"])), dtype=np.int)
-    data["ylabels"] = np.zeros(shape=(len(data['date1']), \
-                                       len(data["city"])), dtype=np.int)
+    hos = np.zeros(shape=(len(dates), len(city)), dtype=np.int)
+    ys = np.zeros(shape=(len(dates), len(city)), dtype=np.int)
 
 
     #insert hospital data
     for i in range(len(newl)):
-        data["hospital"][i] = [newl[i][x] for x in data["city"]]
+        hos[i] = [newl[i][x] for x in city]
 
     #determine the start week of each season: Octorber 01
     for p in range(2009, 2016):
         start=0
-        for j in range(len(data["date1"])):
-            d = data["date1"][j].split('-')
+        for j in range(len(dates)):
+            d = dates[j].split('-')
             if str(p) == d[0] and d[1] == '10' and start == 0:
-                data["season_start"].append(j)
+                ss.append(j)
                 start = 1
-    data["season_start"] = [0] + data["season_start"]
-    data["season_start"] = data["season_start"] + [len(data["date1"])-1]
+    ss = [0] + ss
+    ss = ss + [len(dates)-1]
 
     #label and insert the "start week" to ylabel
-    for j in range (len(data["city"])):
-        for m in range(len(data["season_start"])-1):
+    for j in range (len(city)):
+        for m in range(len(ss)-1):
             had_one=0
-            for i in range(data["season_start"][m], data["season_start"][m+1]-1):
-                if data["hospital"][i,j]>0 \
-                       and data["hospital"][i+1,j] > data["hospital"][i,j] \
-                       and data["hospital"][i+2,j] > data["hospital"][i,j] \
-                       and had_one==0:
-                    data["ylabels"][i,j] = 1
+            for i in range(ss[m], ss[m+1]-1):
+                if hos[i,j]>0 and hos[i+1,j] > hos[i,j] and hos[i+2,j] > hos[i,j] \
+                   and had_one==0:
+                    ys[i,j] = 1
                     had_one=1
-                else:    data["ylabels"][i,j] = 0
+                else:    ys[i,j] = 0
                 
-    data["X"] = np.zeros(shape=(len(data["date1"]), 2), dtype=np.int)
-    city_id = data["city"].index(paras['city'])
-    data["X"][:,0] = data["hospital"].T[city_id]
-    data["X"][:,1] =[x*x for x in data["hospital"].T[city_id]] 
+    X = np.zeros(shape=(len(dates), 2), dtype=np.int)
+    city_id = city.index(paras['city'])
+    X[:,0] = hos.T[city_id]
+    X[:,1] =[x*x for x in hos.T[city_id]] 
 
-    data["y"] = np.zeros(shape=(len(data["date1"]), 1), dtype=np.int)
-    data["y"] = data["ylabels"].T[city_id]
+    y = np.zeros(shape=(len(dates), 1), dtype=np.int)
+    y = ys.T[city_id]
+
+    data["hospital"] = hos
+    data["X"] = X
+    data["y"] = y
+    data["ylabels"] = ys
+
+
+
+
+#@data: data structure holding all data
+#@address1, file address of hospital data
+#@address2, file address of weather data
+
+def load_data(paras, data, address1, address2):
+    load_hospital_data(paras, data, address1)
+
